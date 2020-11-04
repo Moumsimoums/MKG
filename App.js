715818a9ce00e6a135adbeb1a59e609be9f49b25
Antoine Almoussa <antoinealmoussa@antoine.almoussa>
2020-11-04 16:09:15 +0100

    Computer 2 V1

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Allez on va au parc yolo sa m</Text>
+      <Text>Allez on va au parc yolo sa maman</Text>
       <StatusBar style="auto" />
     </View>
   );

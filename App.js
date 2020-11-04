03b8b31c3a90fda2e1af365a32f70d6498b1f8ea
Antoine Almoussa <antoinealmoussa@gmail.com>
2020-11-04 22:59:25 +0100

    styles

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Search from './Components/Search'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Allez on va au parc yolo sa maman le putain d'avion de chasse</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Search />
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

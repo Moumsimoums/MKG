c772e70ab019d9209584742f4333a9d168eeb248
Antoine Almoussa <antoinealmoussa@gmail.com>
2020-11-04 14:42:13 +0100

    Computer 1 update

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Allez on va au parc</Text>
+      <Text>Allez on va au parc yolo</Text>
       <StatusBar style="auto" />
     </View>
   );

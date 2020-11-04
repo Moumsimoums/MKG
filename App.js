33b425d403068f4b63323ec5ba5c50b471b2c7af
Antoine Almoussa <antoinealmoussa@antoine.almoussa>
2020-11-04 16:12:00 +0100

    yolo

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Allez on va au parc yolo sa maman</Text>
+      <Text>Allez on va au parc yolo sa maman le pu</Text>
       <StatusBar style="auto" />
     </View>
   );

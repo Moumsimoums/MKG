3ee0eeeac03804bc95da4e75f958682b966ec6f4
antoinealmoussa <66774890+antoinealmoussa@users.noreply.github.com>
2024-05-22 08:52:53 +0200

    Modify button color

--- Components/Button.js
+++ Components/Button.js
@@ -1,50 +1,47 @@
-import React from 'react'
-import {
-  TouchableOpacity,
-  StyleSheet,
-  View,
-  Text
-} from 'react-native'
+import React from "react";
+import { TouchableOpacity, StyleSheet, View, Text } from "react-native";
 
 class Button extends React.Component {
+    render() {
+        const params = this.props;
+        return (
+            <TouchableOpacity
+                style={style.button}
+                disabled={params.disabled}
+                onPress={() => {
+                    this.handleOnPress(params);
+                }}
+            >
+                <View style={style.buttonLabelContainer}>
+                    <Text style={style.buttonLabel}>{params.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
 
-  render() {
-    const params = this.props
-    return (
-      <TouchableOpacity style={style.button} disabled={params.disabled} onPress={() => {this.handleOnPress(params)}}>
-        <View style={style.buttonLabelContainer}>
-          <Text style={style.buttonLabel}>
-            {params.title}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    )
-  }
-
-  handleOnPress(params) {
-    this.props.onPress(params.scene, params.character)
-  }
+    handleOnPress(params) {
+        this.props.onPress(params.scene, params.character);
+    }
 }
 
-
 const style = StyleSheet.create({
-  button: {
-    flex: 1,
-    borderRadius: 25,
-    backgroundColor: '#EE8700'
-  },
+    button: {
+        flex: 1,
+        borderRadius: 25,
+        backgroundColor: "#EE8772",
+    },
 
-  buttonLabel: {
-    color: '#FFFFFF',
-    fontWeight: 'bold'
-  },
+    buttonLabel: {
+        color: "#FFFFFF",
+        fontWeight: "bold",
+    },
 
-  buttonLabelContainer: {
-    flex: 1,
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-})
+    buttonLabelContainer: {
+        flex: 1,
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
 
-export default Button
+export default Button;
